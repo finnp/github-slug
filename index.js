a8a17834ac25c4cf07c0acd9ce0c8f38af1ee3a9
Finn <derfinn@gmail.com>
2014-07-20 19:54:40 +0200

    Remove console.error

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var urlParse = require('url').parse;
 var url = urlFromGit(url);
 module.exports = function (path, cb) {
     originUrl(path, function (err, url) {
-    if(err) return console.error(err);
       if(err) return cb(err);
       var url = urlFromGit(url);
       var slug = urlParse(url).path.slice(1);
