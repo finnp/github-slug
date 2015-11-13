85d62382f38d231fe745d929def739409b876893
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-11-13 19:17:52 +0000

    Use git-remote-url instead of git-remote-origin-url to allow getting the slug of any remote

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var originUrl = require('git-remote-origin-url')
+var remoteUrl = require('git-remote-url')
 var urlFromGit = require('github-url-from-git')
 var urlParse = require('url').parse
 
-module.exports = function (path, cb) {
-  originUrl(path, function (err, url) {
-    if (err) return cb(err)
+module.exports = function (path, remote, cb) {
+  if (typeof remote === 'function') {
+    cb = remote;
+    remote = 'origin';
+  }
+
+  remoteUrl(path, remote).then(function(url) {
     url = urlFromGit(url)
-    if (typeof url !== 'string') return cb(new Error('No GitHub URL in remote "origin"'))
+    if (typeof url !== 'string') return cb(new Error('No GitHub URL in remote "' + remote + '"'))
     var slug = urlParse(url).path.slice(1)
     cb(null, slug)
-  })
+  }, cb);
 }
